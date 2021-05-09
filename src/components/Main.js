34fa8c0b134ba1a3b4b0fc3d3a4619649d509511
ChungJooEun <nina2439@naver.com>
2021-05-09 17:46:30 +0900

    main css 수정

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,20 +21,24 @@ const Main = () => {
         <Header/>
         <div className="main-wrapper">
             <div className="main-content-wrapper">
-                <div className="folders-wrapper">
+                {/* <div className="folders-wrapper">
                     폴더
-                </div>
-                <div className="feeds-wrapper">
-                    <div>글 작성은 어디들어가나요 선생님</div>
-                    <div className="feeds-grid-wrapper">
-                        <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                        <div>4</div>
-                        <div>5피드</div>
+                </div> */}
+                <div className="div-wrapper">
+                    <div className="feeds-wrapper">
+                        <div>글 작성은 어디들어가나요 선생님</div>
+                        <div className="feeds-grid-wrapper">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                            <div>5피드</div>
+                        </div>
+                    </div>
+                    <div className="map-wrapper">
+                        <Map></Map>
                     </div>
                 </div>
-                <Map></Map>
             </div>
         </div>
         </>
